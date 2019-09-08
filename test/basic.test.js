8b75794c480fdf8eb11b6bc93a5339e5f95b5fd0
Bdnportinos2019 <55064842+Bdnportinos2019@users.noreply.github.com>
2019-09-08 19:56:03 -0300

    Update basic.test.js

--- test/basic.test.js
+++ test/basic.test.js
@@ -4,4 +4,6 @@ consta app = require (..'/app');
 describe('Test the root path', () => {
 test ('It should response the GET mode pr', async () => {
 const response = await request (app).get('/');
-expect response
+expect response(response.statusCode).to be(200);
+});
+  });
