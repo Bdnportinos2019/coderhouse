393f9a6c3657f7e28e8ad1278cd96157b9ebdf43
Bdnportinos2019 <55064842+Bdnportinos2019@users.noreply.github.com>
2019-09-08 19:15:13 -0300

    Create basic.test.js

---
+++ test/basic.test.js
@@ -0,0 +1,7 @@
+const request = requiere ('supertest');
+consta app = require (..'/app');
+
+describe('Test the root path', () => {
+test ('It should response the GET mode pr', async () => {
+const response = await request (app).get('/');
+expect response
